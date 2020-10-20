1367e6ca2c9eabcd6f5ae7a80fceb6ae39962d65
huriel2g <hurieluv2ged93@gmail.com>
2020-10-20 13:12:03 -0600

    add entrada

---
+++ Ejemplo_Gramatica_Jison/entrada.java
@@ -0,0 +1,23 @@
+class id {
+
+    double a;
+    int b = 15-6;
+
+    int funcion(){
+        int z;
+        double w = 69*2;
+
+        if(1+1){
+            double a2;
+            print ("el numero es 1");
+            if(1+1){
+                double a2;
+                print ("el numero es 1");
+                if(1+1){
+                    double a2;
+                    print ("el numero es 1");
+                }
+            }
+        }
+    }
+}
